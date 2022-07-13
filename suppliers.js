e03c50b79d1d64349f2b31160b4502b90bcdefd8
spoonsdave <67965458+spoonsdave@users.noreply.github.com>
2022-07-13 10:57:20 +0100

    add x2

---
+++ suppliers.js
@@ -0,0 +1,181 @@
+function cased() {
+    var x = document.getElementById("cased");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>Diamond<p>Enfield Print Finishers<p>Allsopps<p>4Bind<p>Gomer<p>Green Street Bindery<p>Blissetts<p>Cliftons<p>Reflections(J Muir)<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function crease() {
+    var x = document.getElementById("crease");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>GB Cutting & Creasing<p>Blocking Services<p>Kaps<p>Celloglas<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function encaps() {
+    var x = document.getElementById("encaps");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>Express Encapsulating<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function envelopes() {
+    var x = document.getElementById("envelopes");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>Mailmarque Envelopes<p>Envoprint<p>Gowise<p>The Envelope Works<p>Denmorepress<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function foiling() {
+    var x = document.getElementById("foiling");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>GB Cutting & Creasing<p>Blocking Services<p>Reflections<p>Mirror Finish<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function folding() {
+    var x = document.getElementById("folding");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>Andrews Infinity<p>Folio<p>Burroughs<p>NBC Print Finishing<p>Clinical<p>Enfield Print Finishers<p>Kaps<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function formes() {
+    var x = document.getElementById("formes");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>Farr Formes<p>Rapid Ruling<p>Cook Brothers (Sildie) - BLOCKS<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function gumming() {
+    var x = document.getElementById("gumming");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>Folio<p>Andrews Infinity<p>Kaps<p>Packaids<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function handfin() {
+    var x = document.getElementById("handfin");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>Carol<p>Kaps<p>Enfield Print Finishers<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function labels() {
+    var x = document.getElementById("labels");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>Anglia Labels<p>Abbey Labels<p>UK Labels<p>Riverside Print Services (Sheet label printing)<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function laminate() {
+    var x = document.getElementById("laminate");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>GB Cutting & Creasing<p>Celloglas<p>Reflections<p>Mirror Finish<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function ncr() {
+    var x = document.getElementById("ncr");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>Riverside Print Services<p>Hollinger Print<p>Gowise<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function padding() {
+    var x = document.getElementById("padding");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>Riverside Print Services<p>Gowise<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function perfect() {
+    var x = document.getElementById("perfect");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>Folio<p>Allsopps<p>Andrews Infinity<p>Burroughs<p>Diamond<p>4Bind<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function spotuv() {
+    var x = document.getElementById("spotuv");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>Celloglas<p>Reflections<p>Mirror Finish<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function slip() {
+    var x = document.getElementById("slip");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>Crownfast<p>W. MacCarthy<p>Clarke Rubicon<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function stitch() {
+    var x = document.getElementById("stitch");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>Folio<p>Burroughs<p>Clinical (small finishing specialists)<p>Enfield Print Finishers (small finishing specialists)<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function string() {
+    var x = document.getElementById("string");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>Stringing 2000<p>Midland Trade<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function thread() {
+    var x = document.getElementById("thread");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>Diamond<p>Allsopps<p>4Bind<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+function wireo() {
+    var x = document.getElementById("wireo");
+    if (x.innerHTML === " ") {
+        x.innerHTML = "<p>Andrews Infinity<p>Apple Binding<p>Gowise<p>Larkbeare<p>---";
+    } else {
+        x.innerHTML = " ";
+    }
+}
+
+
