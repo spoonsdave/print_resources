ecda15d23f6099dea5f17e0efe08e0f4c835c7ba
spoonsdave <67965458+spoonsdave@users.noreply.github.com>
2024-09-24 11:49:17 +0100

    Edit to weight calc
    Edit to weight calc

--- weight.js
+++ weight.js
@@ -21,25 +21,25 @@ form.addEventListener('submit', function (e) {
     const boxquantity = Math.ceil((boxkg / bookweight) * 1000)
     const noofboxes = Math.ceil(quantity / boxquantity)
 
-    // British Standard Pallets - 1200x1000mm - 15 6" boxes per row - 6 boxes high - 75 boxes per pallet - pallet height 7"
-    const noofBSpallets = Math.ceil(noofboxes/75)
+    // British Standard Pallets - 1200x1000mm - 15 6" boxes per row - 6 boxes high - 90 boxes per pallet - pallet height 7"
+    const noofBSpallets = Math.ceil(noofboxes / 90)
     const boxesperBSpallet = Math.ceil(noofboxes / noofBSpallets)
     const BSpalletweight = Math.ceil((boxesperBSpallet * boxkg) + 10)
 
-// Euro Pallets - 1200x800mm - 12 6" boxes per row - 6 boxes high - 72 boxes per pallet - pallet height 7"
-const noofEuropallets = Math.ceil(noofboxes/72)
-const boxesperEuropallet = Math.ceil(noofboxes / noofEuropallets)
-const Europalletweight = Math.ceil((boxesperEuropallet * boxkg) + 10)
+    // Euro Pallets - 1200x800mm - 12 6" boxes per row - 6 boxes high - 72 boxes per pallet - pallet height 7"
+    const noofEuropallets = Math.ceil(noofboxes / 72)
+    const boxesperEuropallet = Math.ceil(noofboxes / noofEuropallets)
+    const Europalletweight = Math.ceil((boxesperEuropallet * boxkg) + 10)
 
-// B1 Pallets - 1020x720mm - 8 6" boxes per row - 6 boxes high - 48 boxes per pallet - pallet height 7"
-const noofB1pallets = Math.ceil(noofboxes/48)
-const boxesperB1pallet = Math.ceil(noofboxes / noofB1pallets)
-const B1palletweight = Math.ceil((boxesperB1pallet * boxkg) + 10)
+    // B1 Pallets - 1020x720mm - 8 6" boxes per row - 6 boxes high - 48 boxes per pallet - pallet height 7"
+    const noofB1pallets = Math.ceil(noofboxes / 48)
+    const boxesperB1pallet = Math.ceil(noofboxes / noofB1pallets)
+    const B1palletweight = Math.ceil((boxesperB1pallet * boxkg) + 10)
 
-// S1 Pallets - 900x640mm - 6 6" boxes per row - 6 boxes high - 36 boxes per pallet - pallet height 7"
-const noofS1pallets = Math.ceil(noofboxes/36)
-const boxesperS1pallet = Math.ceil(noofboxes / noofS1pallets)
-const S1palletweight = Math.ceil((boxesperS1pallet * boxkg) + 10)
+    // S1 Pallets - 900x640mm - 6 6" boxes per row - 6 boxes high - 36 boxes per pallet - pallet height 7"
+    const noofS1pallets = Math.ceil(noofboxes / 36)
+    const boxesperS1pallet = Math.ceil(noofboxes / noofS1pallets)
+    const S1palletweight = Math.ceil((boxesperS1pallet * boxkg) + 10)
 
 
 
@@ -51,21 +51,13 @@ const S1palletweight = Math.ceil((boxesperS1pallet * boxkg) + 10)
         boxquantity + " copies per " + boxkg + "kg box" + "<br>" +
         noofboxes + " boxes in total<br><br>Pallet sizes (based on 8kg 12x9x6 inch boxes)<br><br>" +
         //BS pallets
-        noofBSpallets + " x British Standard Pallets - 1200mm (l) x 1000mm (w) x 1100mm (h)<br>" +
-        boxesperBSpallet + " boxes per pallet - " + 
-        BSpalletweight + "kg per pallet<br><br>" +
+        Math.ceil(noofboxes / 90) + " x British Standard Pallets - 1200mm (l) x 1000mm (w) x 1100mm (h)<br>90 x 8kg boxes per pallet - 730kg per pallet<br><br>" +
         //Euro pallets
-        noofEuropallets + " x Euro Pallets - 1200mm (l) x 800mm (w) x 1100mm (h)<br>" +
-        boxesperEuropallet + " boxes per pallet - " + 
-        Europalletweight + "kg per pallet<br><br>" +
+        Math.ceil(noofboxes / 72) + " x Euro Pallets - 1200mm (l) x 800mm (w) x 1100mm (h)<br>72 x 8kg boxes per pallet - 586kg per pallet<br><br>" +
         //B1 pallets
-        noofB1pallets + " x B1 Pallets - 1020mm (l) x 720mm (w) x 1100mm (h)<br>" +
-        boxesperB1pallet + " boxes per pallet - " + 
-        B1palletweight + "kg per pallet<br><br>" +
+        Math.ceil(noofboxes / 45) + " x B1 Pallets - 1020mm (l) x 720mm (w) x 920mm (h)<br>45 x 8kg boxes per pallet - 370kgkg per pallet<br><br>" +
         //S1 pallets
-        noofS1pallets + " x SRA1 Pallets - 900mm (l) x 640mm (w) x 1100mm (h)<br>" +
-        boxesperS1pallet + " boxes per pallet - " + 
-        S1palletweight + "kg per pallet<br><br>";
+        Math.ceil(noofboxes / 40) + " x SRA1 Pallets - 900mm (l) x 640mm (w) x 920mm (h)<br>40 x 8kg boxes per pallet - 330kg per pallet<br><br>";
 
 });
 
